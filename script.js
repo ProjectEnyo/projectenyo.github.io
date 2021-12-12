d14a26fa54797f6bc8ac9712606eb7429f54548b
unknown <hic.3t.nunc@gmail.com>
2021-12-12 22:37:05 +0200

    Add server stats

---
+++ script.js
@@ -0,0 +1,17 @@
+let pingUrl = "https://eu.mc-api.net/v3/server/ping/pvp.projectenyo.eu"
+
+window.addEventListener('load', function() {
+	fetch(pingUrl)
+		.then(response => response.json())
+		.then(data => {
+			let players = data.players.online
+			let maxPlayers = data.players.max
+			let serverStatus = (data.online == true) ? "Online" : "Offline"
+			let key = Object.keys(data.bukkit_extra.pgm)[0]
+			let map = data.bukkit_extra.pgm[key].map.name
+			document.getElementById("online-status").innerHTML = serverStatus
+			document.getElementById("players-online").innerHTML = players + "/" + maxPlayers
+			document.getElementById("current-map").innerHTML = map
+		})
+		.catch(error => console.log(error))
+})
