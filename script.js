9668503b200cc9e924e1481b1f131772b5f886f8
unknown <hic.3t.nunc@gmail.com>
2021-12-12 22:55:03 +0200

    Remove unnecessary variables

--- script.js
+++ script.js
@@ -4,14 +4,10 @@ window.addEventListener('load', function() {
 	fetch(pingUrl)
 		.then(response => response.json())
 		.then(data => {
-			let players = data.players.online
-			let maxPlayers = data.players.max
-			let serverStatus = (data.online == true) ? "Online" : "Offline"
 			let key = Object.keys(data.bukkit_extra.pgm)[0]
-			let map = data.bukkit_extra.pgm[key].map.name
-			document.getElementById("online-status").innerHTML = serverStatus
-			document.getElementById("players-online").innerHTML = players + "/" + maxPlayers
-			document.getElementById("current-map").innerHTML = map
+			document.getElementById("online-status").innerHTML = data.online ? "Online" : "Offline"
+			document.getElementById("players-online").innerHTML = data.players.online + "/" + data.players.max
+			document.getElementById("current-map").innerHTML = data.bukkit_extra.pgm[key].map.name
 		})
 		.catch(error => {
 			document.getElementById("online-status").innerHTML = "Offline"
